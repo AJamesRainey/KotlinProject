e7853fdd1db5416c707614d80f3221b220de44c4
james rainey <64215312+AJamesRainey@users.noreply.github.com>
2022-07-18 16:35:04 -0600

    wrote program

--- src/main/kotlin/Fruit.kt
+++ src/main/kotlin/Fruit.kt
@@ -1,2 +1,38 @@
 class Fruit {
+    val fruitList = mutableSetOf<String>()
+
+    // function to add fruit to list
+fun addFruit(addition: String) {
+        //if size of fruitlist > 10, do not add to list
+    if (fruitList.size >= 10) {
+        println("You have too many fruits already!")
+
+    }
+    else {
+        fruitList.add(addition)
+        println("$addition was added to the fruit list!")
+    }
+    }
+
+
+    fun listFruit() { //list all fruits in fruitList
+        for(items in fruitList){
+            println(items)
+        }
+    }
+
+    fun deleteFruit(deletion: String) { // delete fruit based on user input
+        if (deletion == "all") {
+        }
+        else if (fruitList.contains(deletion)){
+            fruitList.removeAll(listOf(deletion).toSet())
+        }
+        else {
+            println("No such fruit!")
+
+
+
+
+        }
+    }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,29 @@
-fun main(args: Array<String>) {
+fun main() {
+    // Print Hello World
     println("Hello World!")
+    val f1 = Fruit()
+    f1.addFruit("Komquat")
+    f1.deleteFruit(("Komquat"))
+    f1.listFruit()
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    fun javaFacts() {
+        println("Unlike Java, Kotlin does not require the use of checked exceptions using try and catch.")
+        println("Kotlin, unlike Java, does not always require the declaration of datatype for newly created variables. " +
+                "Based on the data being assigned, Kotlin may be able to automatically determine whether a variable is " +
+                "a string or an int for example.  ")
+    }
+
+    fun listsVSArrays() {
+        println("In this exploration of Kotlin, I realized I didn't understand the difference between lists and arrays well." +
+                "Since I already created a mutable list that is used to store fruit, I will now use an array. ")
+    }
+
+    fun createArray() {
+        val fruitWarehouse = arrayOf<Fruit>()
+
+
+    }
+    fun filtering() {
+        println("Predicates are used to filter data")
+    }
 }
